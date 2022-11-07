bc3a59e0842e15e4b1cf74f6ac0828954e9f6065
julia <juliamfbrasileiro21@gmail.com>
2022-11-07 16:01:25 -0300

    Basic Attributes

---
+++ app/src/main/java/com/example/kotlinbasics/BasicCommands.kt
@@ -0,0 +1,37 @@
+package com.example.kotlinbasics
+
+fun main(){
+    // region Immutable Variable
+    // TODO: Add new functionalities
+    var favoriteColor = "Blue"
+
+    // Declare Variables
+    // String
+    val myString: String = "Wolf's Force"
+    var firstCharInString = myString[0]
+    var lastCharInString = myString[myString.length - 1]
+
+    // Int
+    val myInt: Int = 21
+    // Byte
+    val myByte: Byte = 21
+    // Short
+    val myShort: Short = 11111
+    // Long
+    val myLong: Long = 1111111111111111111
+    // Float
+    val myFloat: Float = 21.72f
+    // Double
+    val myDouble: Double = 21.728197
+    // Boolean
+    var isWinter: Boolean = false
+    isWinter = true
+    // Char
+    val myCharLetter: Char = 'J'
+    val myCharNumber: Char = '7'
+
+    println("First character " + firstCharInString)
+    println("Last character " + lastCharInString)
+
+    //endregion
+}
