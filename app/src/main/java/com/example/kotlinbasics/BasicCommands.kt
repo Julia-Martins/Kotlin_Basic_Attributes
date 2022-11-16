2d6b83efef3762c17de0b230ef8bbbc3fd19aebc
julia <juliamfbrasileiro21@gmail.com>
2022-11-16 17:00:06 -0300

    Changes effects

--- app/src/main/java/com/example/kotlinbasics/BasicCommands.kt
+++ app/src/main/java/com/example/kotlinbasics/BasicCommands.kt
@@ -36,7 +36,7 @@ fun main(){
     //endregion
 
     //region Template
-        println("The length of myString is ${myString.length} letters")
+//        println("The length of myString is ${myString.length} letters")
     //endregion
 
     // region Arithmetic Operators  (+,    -,    *,   /,     %)
@@ -44,27 +44,27 @@ fun main(){
     // Multiplication
     var result0 = 5+2f
     result0 *= 2
-    println(result0)
+//    println(result0)
 
     // Division
     var result1 = 5+2f
     result1 /= 2
-    println(result1)
+//    println(result1)
 
     // Plus equal
     var result2 = 5+2f
     result2 += 2
-    println(result2)
+//    println(result2)
 
     // Minus
     var result3 = 5+2f
     result3 -= 2
-    println(result3)
+//    println(result3)
 
     // Modulo
     var result4 = 5+2f
     result4 = 21%2f
-    println(result4)
+//    println(result4)
 
     //endregion
 
@@ -74,19 +74,19 @@ fun main(){
 
     // ==
     val isEqual = a==b
-    println("The result is $isEqual")
+//    println("The result is $isEqual")
 
     // !=
     val isNotEqual = a != b
-     println("The result is $isNotEqual")
+//     println("The result is $isNotEqual")
 
     // < or <=
-    println("The result is ${7 < -21}")
-    println("The result is ${7 <= 21}")
+//    println("The result is ${7 < -21}")
+//    println("The result is ${7 <= 21}")
 
     // > or >=
-    println("The result is ${7 > 21}")
-    println("The result is ${7 >= -21}")
+//    println("The result is ${7 > 21}")
+//    println("The result is ${7 >= -21}")
     //endregion
 
     // region Assignment operators (+=,     -=,     *=,     /=,     %=)
@@ -94,40 +94,40 @@ fun main(){
     // +=
     var myNum = 7
     myNum += 7
-    println("$myNum")
+    //println("$myNum")
 
         //region Increments & Decrements    (++,    --)
 
         // ++
         myNum++
-        println("Printing with ++ $myNum")
+//        println("Printing with ++ $myNum")
 
         ++myNum
-        println("Printing with ++ in other side $myNum")
+//        println("Printing with ++ in other side $myNum")
 
         // --
         myNum--
-        println("Printing with -- $myNum")
+//        println("Printing with -- $myNum")
 
         --myNum
-        println("Printing with -- in other side $myNum")
+//        println("Printing with -- in other side $myNum")
         // endregion
 
     // -=
     myNum -= 7
-    println("$myNum")
+//    println("$myNum")
 
     // *=
     myNum *= 7
-    println("$myNum")
+//    println("$myNum")
 
     // /=
     myNum /= 7
-    println("$myNum")
+//    println("$myNum")
 
     // %=
     myNum %= 7
-    println("$myNum")
+//    println("$myNum")
 
     //endregion
 
@@ -136,11 +136,11 @@ fun main(){
     val heightPerson20 = 189
 
     if(heightPerson10  > heightPerson20){
-        println("Person 10 is taller than Person 20")
+//        println("Person 10 is taller than Person 20")
     }else if(heightPerson10 == heightPerson20){
-        println("Person 10 is equal than Person 20")
+//        println("Person 10 is equal than Person 20")
     }else{
-        println("Person 20 is taller than Person 10")
+//        println("Person 20 is taller than Person 10")
     }
 
     // Exercise
@@ -151,61 +151,88 @@ fun main(){
     val driveAge = 16
 
     if(age >= drinkAge){
-        println("You have $age")
-        println("Now you can do whatever you want, but you wil pay the consequences")
+//        println("You have $age")
+//        println("Now you can do whatever you want, but you wil pay the consequences")
     }else if(age >= voteAge){
-        println("You have $age")
-        println("Now you can vote in a candidate")
+//        println("You have $age")
+//        println("Now you can vote in a candidate")
     }else if(age >= driveAge){
-        println("You have $age")
-        println("Now you can drive, but be careful to crash the vehicular")
+//        println("You have $age")
+//        println("Now you can drive, but be careful to crash the vehicular")
     }else{
-        println("You can't do anything yet, just wait some time for you grow up")
+//        println("You can't do anything yet, just wait some time for you grow up")
     }
 
     //endregion
 
     // region When Expressions (When)
-    var season = 3
+//    var season = 3
 
-    when(season){
-        1 -> println("Spring")
-        2 -> println("Summer")
-        3 ->{
-            println("Fall")
-            println("Autumn")
-        }
-        4 -> println("Winter")
-        else -> println("Invalid Season")
-    }
+//    when(season){
+//        1 -> println("Spring")
+//        2 -> println("Summer")
+//        3 ->{
+//            println("Fall")
+//            println("Autumn")
+//        }
+//        4 -> println("Winter")
+//        else -> println("Invalid Season")
+//    }
 
-    var month = 3
-    when(month){
-        in 3..5 -> println("Spring")
-        in 6..8 -> println("Summer")
-        in 9..11 -> {
-            println("Fall")
-            println("Autumn")
-        }
-        in 12 downTo 2 -> println("Winter")
-        else -> println("Invalid Month")
-    }
+//    var month = 3
+//    when(month){
+//        in 3..5 -> println("Spring")
+//        in 6..8 -> println("Summer")
+//        in 9..11 -> {
+//            println("Fall")
+//            println("Autumn")
+//        }
+//        in 12 downTo 2 -> println("Winter")
+//        else -> println("Invalid Month")
+//    }
 
-    when(age){
-        !in 0..20 -> println("Now you may drink in US")
-        in 18..20 -> println("Now you may vote in so many countries")
-        in 16..17 -> println("Now you may drive in US")
-        else -> println("You're too young")
-    }
+//    when(age){
+//        !in 0..20 -> println("Now you may drink in US")
+//        in 18..20 -> println("Now you may vote in so many countries")
+//        in 16..17 -> println("Now you may drive in US")
+//        else -> println("You're too young")
+//    }
 
-    var x : Any = 21.7f
+//    var x : Any = 21.7f
 
-    when(x){
-        is Int -> println("$x is an Int")
-        is Double -> println("$x is a Double")
-        is String -> println("$x is a String")
-        else -> println("$x is none of the above")
-    }
+//    when(x){
+//        is Int -> println("$x is an Int")
+//        is Double -> println("$x is a Double")
+//        is String -> println("$x is a String")
+//        else -> println("$x is none of the above")
+//    }
 
     // endregion
+
+    //region Loop(While/ Do While)
+
+    var x = 21
+    while(x >= 0){
+        //println("$x")
+        x -= 2
+    }
+
+    x = 100
+    do{
+//        println("$x")
+        x--
+    }while (x >= 90)
+
+    var feltTemp = "cold"
+    var roomTemp = -10
+
+    while(feltTemp == "cold"){
+        roomTemp++
+        if(roomTemp >= 20){
+            feltTemp = "Comfy"
+            println("It's comfy now")
+        }
+    }
+
+    //endregion
 }
